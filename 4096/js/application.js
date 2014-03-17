b5867e78227aa67175d404044ca34c42dc277f35
rishirajsurti <rishirajsurti.iitm@gmail.com>
2014-03-17 22:22:42 +0530

    4096

---
+++ 4096/js/application.js
@@ -0,0 +1,12 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(
+  function () {
+    new GameManager(
+      4,
+      KeyboardInputManager,
+      HTMLActuator,
+      LocalScoreManager,
+      BottomlessStack
+    );
+  }
+);
---
+++ 4096/js/tile.js
@@ -0,0 +1,23 @@
+function Tile(position, value) {
+  this.x                = position.x;
+  this.y                = position.y;
+  this.value            = value || 2;
+
+  this.previousPosition = null;
+  this.mergedFrom       = null; // Tracks tiles that merged together
+}
+
+Tile.prototype.clone = function () {
+  return new Tile ({x: this.x,
+                    y: this.y},
+                   this.value);
+};
+
+Tile.prototype.savePosition = function () {
+  this.previousPosition = { x: this.x, y: this.y };
+};
+
+Tile.prototype.updatePosition = function (position) {
+  this.x = position.x;
+  this.y = position.y;
+};
